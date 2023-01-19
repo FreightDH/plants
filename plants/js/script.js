ffa13fd6b037d3ae77c136eb91b6429c5c440cda
Max Zolotkov <maxim.zolotkov@yandex.ru>
2023-01-19 23:45:31 +0300

    feat: update burger

--- plants/js/script.js
+++ plants/js/script.js
@@ -13,10 +13,25 @@ function burger() {
 			}
 			return false;
 		}); 
+		// if ($('.header-menu__icon').css('display') === 'block') {
+		// 	$(document).on('click', '.header-menu__menu', function(event) {
+		// 		if ($('.header-menu__icon').hasClass('active')) {
+		// 			$('.header-menu__menu').removeClass('active');
+		// 			$('.header-menu__icon').removeClass('active');
+		// 			$('html, body').css('overflow', '');
+		// 		}
+		// 		else {
+		// 			$('.header-menu__menu').addClass('active');
+		// 			$('.header-menu__icon').addClass('active');
+		// 			$('html, body').css('overflow', 'hidden');
+		// 		}
+		// 		return false;
+		// 	});
+		// }
 	});
 }
 
-function linkClick() {
+function burgerClose() {
 	$(document).ready(function () {
 		$(document).on('click', '.header-menu-list__link', function(event) {
 			if ($('.header-menu__menu').hasClass('active')) {
@@ -25,8 +40,18 @@ function linkClick() {
 				$('html, body').css('overflow', '');
 			}
 		});
+		
+		if ($('.header-menu__icon').css('display') === 'block') {
+			$(document).on('click', '.header-menu__menu', function(event) {
+				if ($('.header-menu__menu').hasClass('active')) {
+					$('.header-menu__menu').removeClass('active');
+					$('.header-menu__icon').removeClass('active');
+					$('html, body').css('overflow', '');
+				}
+			});
+		}
 	});
 }
 
 burger();
-linkClick();
+burgerClose();
