3c451f81b9ed4da9ac5ca3f8bed72786a6ab867f
FreightDH <maxim.zolotkov@yandex.ru>
2023-02-27 03:32:44 +0300

    feat: add popup with link to deploy

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -1,21 +1,21 @@
 const playList = [
 	{      
 	  title: 'Aqua Caelestis',
-	  src: '/momentum/assets/sounds/Aqua Caelestis.mp3',
+	  src: '../assets/sounds/Aqua Caelestis.mp3',
 	  duration: '00:58'
 	},  {      
 	  title: 'River Flows In You',
-	  src: '/momentum/assets/sounds/River Flows In You.mp3',
+	  src: '../assets/sounds/River Flows In You.mp3',
 	  duration: '03:50'
 	},
 	{      
 	  title: 'Summer Wind',
-	  src: '/momentum/assets/sounds/Summer Wind.mp3',
+	  src: '../assets/sounds/Summer Wind.mp3',
 	  duration: '05:05'
 	},
 	{      
 	  title: 'Ennio Morricone',
-	  src: '/momentum/assets/sounds/Ennio Morricone.mp3',
+	  src: '../assets/sounds/Ennio Morricone.mp3',
 	  duration: '05:03'
 	}
 ]
